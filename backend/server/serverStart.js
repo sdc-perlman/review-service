064f883fa2886865d814c7ffbe44eb8c38b2ee19
Dane Murphy <danem7@gmail.com>
2021-03-14 15:54:43 -0700

    adjustments with calling of db.sync()

--- backend/server/serverStart.js
+++ backend/server/serverStart.js
@@ -1,8 +1,7 @@
+require('./db/relationships');
 const db = require('./db/index');
 
-(async () => {
-	await db.sync();
-	const app = require('./server');
-	const PORT = process.env.PORT || 5002;
-	app.listen(PORT, () => console.log(`Review service running on ${PORT}`));
-})();
+db.sync();
+const app = require('./server');
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => console.log(`Review service running on ${PORT}`));
