9c30859c2977c5324706aa5bbd091f8467c5e3e7
Dane Murphy <danem7@gmail.com>
2021-03-18 23:27:09 -0700

    moved client to proxy for server side rendering

--- client/src/index.jsx
+++
@@ -1,5 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-import './index.scss';
-import ReviewsService from './components/ReviewService.jsx';
-
-ReactDOM.hydrate(<ReviewsService />, document.getElementById('reviews'));
